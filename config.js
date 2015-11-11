4d3d20dabf2085a508dcd1d11dc78ddc3e65a8b9
李江 <lijiang@sina.com>
2015-11-11 16:19:17 +0800

    增加性能监控

--- config.js
+++ config.js
@@ -34,7 +34,15 @@ var config = {
 };
 
 config[env].params = {
-    title:'洋码头-聚洋货'
+    title:'洋码头-聚洋货',
+    monitorConfig:{
+       appId:'preorder.app.ymatou.com',        //项目名
+       host: '172.16.100.13',           //提交到服务器地址 //10.10.15.239
+       port:8089,                             //提交到服务器端口 //9095
+       path:'/api/perfmon/',              //提交到服务器路径
+       open:true,                             //是否开启
+       loopTime:30000                   //上传耗时间隔  Default:30s
+    }
 }
 
 module.exports = config[env];
