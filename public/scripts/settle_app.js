32007d0405ca79174162ee0464c182b9dfb2d3ad
unknown <lijiang@ymt.corp>
2016-01-05 11:08:34 +0800

    修复取消优惠券 继续显示优惠信息

--- public/scripts/settle_app.js
+++ public/scripts/settle_app.js
@@ -335,6 +335,7 @@
                     }else{
                         $scope.confirmInputCoupon(order);
                     }
+                    acountDiscount();
                 }
             }
 
